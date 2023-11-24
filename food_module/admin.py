4218bd0084894842427e164e92f2923175ff39f8
zahramousavi930 <zahramousavi930@gmail.com>
2023-11-24 20:18:30 +0330

    show models in admin

--- food_module/admin.py
+++ food_module/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+class Food_display(admin.ModelAdmin):
+    list_display = ('food_name','is_active','price')
+    list_editable = ('is_active','price')
+
+
+class reserv_display(admin.ModelAdmin):
+    list_display = ('name','number_of_guests','date','timee')
+
+
+admin.site.register(models.Food_menu,Food_display)
+admin.site.register(models.reservation,reserv_display)
+admin.site.register(models.Classification_food)
+admin.site.register(models.Footer_data)
