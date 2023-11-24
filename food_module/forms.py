a4a7a501f69ebcb38e79bc4f8b20a592a5f01f4e
zahramousavi930 <zahramousavi930@gmail.com>
2023-11-24 20:09:54 +0330

    add Reservation form

---
+++ food_module/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.core import validators
+
+
+
+
+
+class Reservation(forms.Form):
+    name =forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder' :'name' ,'class' :'form-control'}),
+        validators=[
+            validators.MaxLengthValidator(100)
+        ]
+    )
+
+    phone =forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder' :'phone' ,'class' :'form-control'}),
+        validators=[
+            validators.MaxLengthValidator(100)
+        ]
+    )
+
+    email = forms.EmailField(
+
+        widget=forms.EmailInput(
+            attrs={'placeholder': 'email', 'class': 'form-control',}),
+        validators=[
+            validators.MaxLengthValidator(100),
+            validators.EmailValidator
+        ]
+    )
+
