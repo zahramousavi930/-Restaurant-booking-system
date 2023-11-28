0287149622c1b2ae1f4d785396fb377cea032d5d
zahramousavi930 <zahramousavi930@gmail.com>
2023-11-28 16:22:15 +0000

    modify settings

--- resturant/settings.py
+++ resturant/settings.py
@@ -135,15 +135,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT =  '/static/'
+MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_URL = '/medias/'
 
-COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
