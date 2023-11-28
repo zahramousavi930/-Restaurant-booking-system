82c4faa0062ef7ed090dca53526a95a07be3937f
zahramousavi930 <zahramousavi930@gmail.com>
2023-11-28 16:14:11 +0000

    modify settings

--- resturant/settings.py
+++ resturant/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c@kp4rfp236b4+9_7+2jsaw+b%-0$d8f53kz#1fe*nh)_r#+(+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS =['*']
 # Application definition
