8da478f31f4123db761d9a7825c903a19bd22129
zahramousavi930 <zahramousavi930@gmail.com>
2023-11-28 20:53:44 +0000

    remove

--- resturant/settings.py
+++ resturant/settings.py
@@ -156,7 +156,7 @@ STATICFILES_DIRS = [
 
 
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
