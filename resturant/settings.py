a0708a2e444922bdd500da0d9e90221c0ec86f7a
zahramousavi930 <zahramousavi930@gmail.com>
2023-11-28 16:20:47 +0000

    modify settings

--- resturant/settings.py
+++ resturant/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT =  '/static/'
 
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
