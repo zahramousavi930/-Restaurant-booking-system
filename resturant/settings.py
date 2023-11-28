31e54cea15b34800d67405115c73cbada31b84d3
zahramousavi930 <zahramousavi930@gmail.com>
2023-11-28 10:38:16 +0000

    everything is setup fo heroku

--- resturant/settings.py
+++ resturant/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from pathlib import Path
+import dj_database_url
+
+if os.path.isfile('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,13 +85,16 @@ WSGI_APPLICATION = 'resturant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES={
+     'default':dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 AUTH_USER_MODEL = 'food_module.User'
