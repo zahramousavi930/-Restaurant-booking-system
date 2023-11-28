123fe98f172c0d7dd5af82e2c9b2595167badfc5
zahramousavi930 <zahramousavi930@gmail.com>
2023-11-28 20:52:43 +0000

    add serve file

--- resturant/urls.py
+++ resturant/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path ,include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('food_module.urls')),
     path('user/', include('account_module.urls')),
+     re_path(r'^static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT}),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
