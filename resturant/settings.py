cc39e7e80c6ad6198452b4fe25e1d3ff3cecee6c
zahramousavi930 <zahramousavi930@gmail.com>
2023-11-29 07:45:27 +0000

    add whitenoise and debuge fasle

--- resturant/settings.py
+++ resturant/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c@kp4rfp236b4+9_7+2jsaw+b%-0$d8f53kz#1fe*nh)_r#+(+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS =['django-resturant-dc830834c6f2.herokuapp.com',' https://django-resturant-dc830834c6f2.herokuapp.com/',' https://django-resturant-dc830834c6f2.herokuapp.com',' http://django-resturant-dc830834c6f2.herokuapp.com/']
