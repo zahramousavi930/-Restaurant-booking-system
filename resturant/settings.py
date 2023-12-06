ba57ffb75f19888eba1042d3b17164f4f21f1891
zahramousavi930 <zahramousavi930@gmail.com>
2023-12-06 14:29:36 +0000

    staticfiels

--- resturant/settings.py
+++ resturant/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
  
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'food_module',
     'account_module',
 
@@ -161,7 +161,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 STATIC_URL='/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
 
