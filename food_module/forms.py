5d1b61d3fc755363d6d8d7b83f69734fc7b7aaab
zahramousavi930 <zahramousavi930@gmail.com>
2024-03-05 15:02:39 +0000

    modify forms

--- food_module/forms.py
+++ food_module/forms.py
@@ -1,34 +1,22 @@
 from django import forms
 from django.core import validators
+from .models import reservation
 
 
 
 
-class Reservation(forms.Form):
-    name =forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder' :'name' ,'class' :'form-control'}),
-        validators=[
-            validators.MaxLengthValidator(100)
-        ]
-    )
 
-    phone =forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder' :'phone' ,'class' :'form-control'}),
-        validators=[
-            validators.MaxLengthValidator(100)
-        ]
-    )
+class ReservationForm(forms.ModelForm):
+    class Meta:
+        model = reservation
+        fields = ['name', 'phone', 'email', 'number_of_guests']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder':'name','class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'placeholder':'phone','class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'placeholder':'email','class': 'form-control'}),
+            'number_of_guests': forms.NumberInput(attrs={'placeholder':'number of guests','class': 'form-control'}),
 
-    email = forms.EmailField(
-
-        widget=forms.EmailInput(
-            attrs={'placeholder': 'email', 'class': 'form-control',}),
-        validators=[
-            validators.MaxLengthValidator(100),
-            validators.EmailValidator
-        ]
-    )
-
+        }
 
 
 
