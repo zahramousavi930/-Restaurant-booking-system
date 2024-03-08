198b264c6da0d67e1ef40d59627740b35c035f00
zahramousavi930 <zahramousavi930@gmail.com>
2024-03-08 18:02:12 +0000

    edit statics settings

--- resturant/settings.py
+++ resturant/settings.py
@@ -165,10 +165,10 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
 
 
