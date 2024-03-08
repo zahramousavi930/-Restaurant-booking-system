0ddd3bfa1d767c8deb597d078f66df56a9c3edfd
zahramousavi930 <zahramousavi930@gmail.com>
2024-03-08 17:22:17 +0000

    edit allow host

--- resturant/settings.py
+++ resturant/settings.py
@@ -34,8 +34,7 @@ SECRET_KEY = 'django-insecure-c@kp4rfp236b4+9_7+2jsaw+b%-0$d8f53kz#1fe*nh)_r#+(+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =['resturant-bookingsystem-d1932e1df652.herokuapp.com',
-'8000-zahramousav-restaurantb-jmhffjysote.ws-eu108.gitpod.io',]
+ALLOWED_HOSTS =['resturant-3252-19e8d5d3a135.herokuapp.com']
 
 # Application definition
 
