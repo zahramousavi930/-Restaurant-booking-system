ee35e7b059c7141124391b59bc57fdf7317aebdb
zahramousavi930 <zahramousavi930@gmail.com>
2024-03-14 18:22:04 +0000

    debuge

--- resturant/settings.py
+++ resturant/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-c@kp4rfp236b4+9_7+2jsaw+b%-0$d8f53kz#1fe*nh)_r#+(+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =['https://booking-resturant-28103ce31422.herokuapp.com',
+ALLOWED_HOSTS =['booking-resturant-28103ce31422.herokuapp.com',
                     'https://booking-resturant-28103ce31422.herokuapp.com',
                     'http://booking-resturant-28103ce31422.herokuapp.com',
                     'www.booking-resturant-28103ce31422.herokuapp.com'
